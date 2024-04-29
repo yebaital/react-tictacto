86b44d6886a4e0876fd5d2d7a85f098a04982ea6
Yousef <yousef.baitalmal@gmail.com>
2024-04-29 23:52:56 +0300

    Add Player component and update App.jsx and index.html
    A new Player component is added which allows player's name editing. 'App.jsx' is updated to include the new Player component. Additional elements are added to index.html for enhanced UI, including a header with a game logo and title.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Player from "./components/Player.jsx";
 
 function App() {
-  
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player initialName={"Player 1"} symbol={"X"}/>
+                    <Player initialName={"Player 2"} symbol={"O"}/>
+                </ol>
+                GAME BOARD
+            </div>
+            LOG
+        </main>
+    )
 }
 
 export default App
