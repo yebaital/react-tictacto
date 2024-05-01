2ac734f3f0dddbf94543e19ab9a50f56122bb836
yebaital <yousef.baitalmal@gmail.com>
2024-05-01 12:20:42 +0300

    Add functionality for names instead of symbols to be used to announce the winner or active player.

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,11 @@ const initialGameBoard = [
 
 function App() {
 
+    const [players, setPlayers] = useState({
+        'X': 'Player 1',
+        'O': 'Player 2',
+    })
+
     const [gameTurns, setGameTurns] = useState([]);
 
     const activePlayer = deriveActivePlayer(gameTurns);
@@ -43,7 +48,7 @@ function App() {
         const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
         if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
-            winner = firstSquareSymbol;
+            winner = players[firstSquareSymbol];
         }
     }
 
@@ -63,12 +68,28 @@ function App() {
         setGameTurns([]);
     }
 
+    function handlePlayerNameChange(symbol, newName) {
+        setPlayers(prevPlayers => {
+            return {...prevPlayers, [symbol]: newName};
+        })
+    }
+
     return (
         <main>
             <div id="game-container">
                 <ol id="players" className="highlight-player">
-                    <Player initialName={"Player 1"} symbol={"X"} isActive={activePlayer === 'X'}/>
-                    <Player initialName={"Player 2"} symbol={"O"} isActive={activePlayer === 'O'}/>
+                    <Player
+                        initialName={"Player 1"}
+                        symbol={"X"}
+                        isActive={activePlayer === 'X'}
+                        onChangeName={handlePlayerNameChange}
+                    />
+                    <Player
+                        initialName={"Player 2"}
+                        symbol={"O"}
+                        isActive={activePlayer === 'O'}
+                        onChangeName={handlePlayerNameChange}
+                    />
                 </ol>
                 {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch}/>}
                 <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,16 @@
 import {useState} from "react";
 
-export default function Player({initialName, symbol, isActive}) {
+export default function Player({initialName, symbol, isActive, onChangeName}) {
 
     const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName)
+        }
     }
 
     function handleChange(event) {
@@ -16,7 +20,7 @@ export default function Player({initialName, symbol, isActive}) {
     let editablePlayerName = <span className="player-name">{playerName}</span>
 
     if (isEditing) {
-        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
     }
 
     return (
